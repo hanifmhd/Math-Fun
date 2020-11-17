1dbe63c37473ca66e01fbb22979bbeb68280ec0a
Muhammad Hanif <muhammadhanif.if@gmail.com>
2020-11-17 19:47:41 +0700

    Add screenshots and disable yellowbox

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
-import Routes from "./src/routes";
-import { name as appName } from "./app.json";
+import {AppRegistry, LogBox} from 'react-native';
+import Routes from './src/routes';
+import {name as appName} from './app.json';
 
+LogBox.ignoreAllLogs(true);
 AppRegistry.registerComponent(appName, () => Routes);
